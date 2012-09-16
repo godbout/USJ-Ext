68e3d87c6aba369cf58593aacb12662083983708
Guill Lo <guill.bout@gmail.com>
2012-09-16 22:21:26 +0800

    1st commit

---
+++ USJ Ext/USJ Ext/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  USJ Ext
+//
+//  Created by Guill Lo on 9/16/12.
+//  Copyright (c) 2012 sleeplessmind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
